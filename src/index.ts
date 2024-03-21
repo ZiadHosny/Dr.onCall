85117311e651a8adafab1a9fc01524fd17a04c1a
ziad <ziad.hosny@bitspire.ca>
2024-03-21 13:49:09 +0200

    add app name

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { getFromEnv } from "./utils/getFromEnv.js"
 import { connectToMongoDb } from "./database/connectToMongo.js"
 import { logBlueMsg, logErrMsg } from "./utils/console/log.js"
 import { router } from "./router.js"
+import { APP_NAME } from "./utils/constants.js"
 
 const { port } = getFromEnv()
 
 connectToMongoDb()
 
-router.listen(port, () => logBlueMsg(`Dr OnCall Api listening on port ${port}!`))
+router.listen(port, () => logBlueMsg(`${APP_NAME} Api listening on port ${port}!`))
 
 process.on('unhandledRejection', (err) => {
     logErrMsg("unhandledRejection" + err)
